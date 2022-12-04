76f519b84b45d196556d9570d056171776b23e14
Qin Xia <qx744@utexas.edu>
2022-12-04 14:20:42 -0600

    .

--- js/echarts-1.js
+++ js/echarts-1.js
@@ -1,7 +1,7 @@
 var girth = [8.3, 8.6, 8.8, 10.5, 10.7, 10.8, 11.0, 11.0, 11.1, 11.2, 11.3, 11.4, 11.4, 11.7, 12.0, 12.9];
-//直方图初始化函数，
+，
 function histChart() {
-//发送请求，赋值
+
     $.ajaxSetup({async: false});
     $.ajax({
         url: '/echarts1',
@@ -15,46 +15,44 @@ function histChart() {
     set_hist()
 }
 
-//直方图参数
 function set_hist() {
 
      var histChart = echarts.init(document.getElementById('histChart'));
-    // See https://github.com/ecomfe/echarts-stat
+
     var bins = ecStat.histogram(girth, 'scott');
 
-    // 柱子间间隔的刻度
+
     var interval;
 
     var min = Infinity;
     var max = -Infinity;
 
     var data = echarts.util.map(bins.data, function (item, index) {
-        // 左刻度
+
         var x0 = bins.bins[index].x0;
-        // 右刻度
+
         var x1 = bins.bins[index].x1;
         interval = (x1 - x0).toFixed(2);
-        // 获得数据集中最值
+
         min = Math.min(min, x0);
         max = Math.max(max, x1);
-        // item[0]代表刻度的中间值，item[1]代表出现的次数
+
         return [x0, x1, item[1]];
     });
-     // 自定义渲染效果
+
     function renderItem(params, api) {
-        // 这个根据自己的需求适当调节
+
         var yValue = api.value(2);
         var start = api.coord([api.value(0), yValue]);
         var size = api.size([api.value(1) - api.value(0), yValue]);
         var style = api.style();
 
         return {
-            // 矩形及配置
             type: 'rect',
             shape: {
                 x: start[0],
                 y: start[1],
-                width: size[0],  //图形宽度
+                width: size[0],
                 height: size[1]
             },
             style: style
@@ -92,26 +90,22 @@ function set_hist() {
         }],
         series: [{
             name: 'Number of movies',
-            type: 'custom',  //自定义
-            renderItem: renderItem,  //各种值
+            type: 'custom',
+            renderItem: renderItem, 
             barWidth:30,
             encode: {
-                // 表示将data中的data[0]和data[1]映射到x轴
                 x: [0, 1],
-                // 表示将data中的data[2]映射到y轴
                 y: 2,
-                // 表示将data中的data[2]映射到tooltip
                 tooltip: 2,
-                // 表示将data中的data[2]映射到label
                 label: 2
             },
             data: data,
              itemStyle: {
                 normal: {
                     label: {
-                        show: true, //开启显示
-                        position: 'top', //在上方显示
-                        textStyle: { //数值样式
+                        show: true
+                        position: 'top', 
+                        textStyle: { 
                             color: 'blue',
                             fontSize: 16
                         }
