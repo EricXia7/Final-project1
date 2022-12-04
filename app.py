308325ccc68071a8786c450582982c5bbea106f6
Qin Xia <qx744@utexas.edu>
2022-12-04 13:23:45 -0600

    4,#4

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from util import get_1_data, get_2_data, get_3_data, find_years, find_types, fin
 
 app = Flask(__name__)
 
-#main page
+'''main page'''
 @app.route('/')
 def hello_world():
     years = find_years()
@@ -11,7 +11,7 @@ def hello_world():
     return render_template("index.html", years=years, types=types)
 
 
-#Histogram
+'''Histogram'''
 @app.route('/echarts1', methods=['GET', 'POST'])
 def echarts1():
     form = request.form.to_dict()
@@ -20,3 +20,37 @@ def echarts1():
         return jsonify({"data": data, "msg": True})
     return jsonify({"data": 'Can not find any information', "msg": False})
 
+'''line chart'''
+@app.route('/echarts2', methods=['GET', 'POST'])
+def echarts2():
+    form = request.form.to_dict()
+    data = get_2_data(form)
+    data = [i for i in data if i]
+    if data:
+        data1, data2, data3, data4, years = data
+        return jsonify({"data1": data1, "data2": data2, "data3": data3, "data4": data4, "years": years, "msg": True})
+    return jsonify({"data": 'Can not find any information', "msg": False})
+
+
+'''pie chart'''
+@app.route('/echarts3', methods=['GET', 'POST'])
+def echarts3():
+    form = request.form.to_dict()
+    print(form)
+    data = get_3_data(form)
+    data = [i for i in data if i]
+    if data:
+        types, data = data
+        return jsonify({"types": types, "data": data, "msg": True})
+    return jsonify({"data": "can not find the data", "msg": False})
+
+
+'''connect to the table'''
+@app.route('/info/all', methods=['GET', 'POST'])
+def info_search():
+    data = find_table()
+    return jsonify({"msg": True, "data": data})
+
+
+if __name__ == '__main__':
+    app.run()
